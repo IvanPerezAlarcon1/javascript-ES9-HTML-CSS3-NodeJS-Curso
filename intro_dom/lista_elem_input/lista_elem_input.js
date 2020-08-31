bafad22f5f43ae13017ccbd01d505e95c3a9a2f7
ivan <ivan.perez.alarcon@utem.cl>
2020-08-30 21:56:30 -0400

    Función recursiva render, eliminar elementos ingresados sin que exista problema con los indices de los elementos

--- intro_dom/lista_elem_input/lista_elem_input.js
+++ intro_dom/lista_elem_input/lista_elem_input.js
@@ -1,4 +1,26 @@
 const todos = [];
+
+const render = () => {
+    const todoList = document.getElementById("todo-list");
+        todoList.innerHTML = '';
+        for(let i=0;i<todos.length;i++){
+            todoList.innerHTML = todoList.innerHTML + "<li>" + todos[i] + "</li>";
+        }
+        const todosTemplate = todos.map(t =>"<li>" + t + "</li>");
+        todoList.innerHTML = todosTemplate.join("");
+        console.log(todosTemplate);
+        const elementos = document.querySelectorAll("#todo-list li");
+        elementos.forEach((elemento,i) => {
+            elemento.addEventListener("click", () => {
+                elemento.parentNode.removeChild(elemento);
+                todos.splice(i,1);
+                render();
+                //console.log(todos,i);
+            
+            })
+        })  
+}
+
 window.onload = () => {
     //llamamos al elemento form de HTML mediante su id para utilizarlo
     const form = document.getElementById("todo-form");
@@ -12,16 +34,18 @@ window.onload = () => {
         todo.value = '';//dejar vacío el contenedor
         //ver texto ingresado
         todos.push(todoText);
+
+
+        /*
+        //LA SIMPLIFICACIÓN DE ESTA PORCIÓN DE CÓD ESTÁ MÁS ABAJO Y ACTIVA llamda en la función render()
         //console.log(todoText);
         const todoList = document.getElementById("todo-list");
 
-        /*
         //esta parte del código se puede optimizar con MAP
         todoList.innerHTML = '';//dejar vacío el contenedor para que nose repita el print de los elementos
         for(let i=0;i<todos.length;i++){
             todoList.innerHTML = todoList.innerHTML + "<li>" + todos[i] + "</li>";
         }
-        */
 
         //t es el elemento que se va a iterar según la cantidad de elementos tenga en este caso el arreglo todos
         const todosTemplate = todos.map(t =>"<li>" + t + "</li>");
@@ -30,14 +54,27 @@ window.onload = () => {
         console.log(todosTemplate);
 
         //permite seleccionar por clases, etiquetas, nombres, ids de HTML
-        //En este caso se buscará por ID 
+        //En este caso se buscará por ID, seleccionamos todos los elementos li de #todo-list
         const elementos = document.querySelectorAll("#todo-list li");
+        //iteramos mediante forEach(similar a map), solo que en este caso el resultado no se puede asignar a una constante
         elementos.forEach((elemento,i) => {//itera la variable elementos
-            console.log(elemento,i);
-            //en el evento click, ejecuta algo
+            //muestra todos los elementos, al agregar uno nuevo
+            //console.log(elemento,i);
+
+            //agregamos un evento, en este caso click entonces se ejecuta algo
             elemento.addEventListener("click", () => {
-                console.log(elemento,i);
+                //el padre elimina al hijo cuando se le da click a este hijo
+                elemento.parentNode.removeChild(elemento);
+                //eliminamos el elemento clickqueado del arreglo todos con splice, usando el indice de inicio de la eliminación y el nro de elementos que queremos eliminar
+                todos.splice(i,1);
+                console.log(todos,i);
+
+                //console.log(elemento,i); //muestra el elemento al hacerle click
+            
+            
             })
         })  
+        */
+       render();
     }
 }
